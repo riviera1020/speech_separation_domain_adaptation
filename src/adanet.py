277c532141e245c17cd33e4d32c09e138aba8e55
riviera1020 <riviera1020@gmail.com>
2020-03-27 03:23:59 +0800

    adanet network

--- src/adanet.py
+++ src/adanet.py
@@ -4,6 +4,9 @@ import torch.nn.functional as F
 import itertools
 
 from src.sep_utils import overlap_and_add
+from src.conv_tasnet import Encoder, Decoder, TemporalBlock
+from src.conv_tasnet import DepthwiseSeparableConv, chose_norm
+from src.conv_tasnet import ChannelwiseLayerNorm, Chomp1d
 
 EPS = 1e-8
 
@@ -49,9 +52,17 @@ class ADANet(nn.Module):
                 self.emb_size, self.norm_type, self.causal, self.mask_nonlinear, self.dropout)
         self.decoder = Decoder(self.N, self.L)
         # init
-        for p in self.parameters():
+        init_type = config.get('init', 'xavier_normal')
+        print(f'Use {init_type} init')
+        for name, p in self.named_parameters():
             if p.dim() > 1:
-                nn.init.xavier_normal_(p)
+                if init_type == 'xavier_normal':
+                    nn.init.xavier_normal_(p)
+                elif init_type == 'xavier_uniform':
+                    nn.init.xavier_uniform_(p)
+                else:
+                    print('Specify init method')
+                    exit()
 
     def forward(self, mixture):
         """
@@ -69,55 +80,6 @@ class ADANet(nn.Module):
         T_conv = est_source.size(-1)
         est_source = F.pad(est_source, (0, T_origin - T_conv))
         return est_source
-
-
-class Encoder(nn.Module):
-    """Estimation of the nonnegative mixture weight by a 1-D conv layer.
-    """
-    def __init__(self, L, N):
-        super(Encoder, self).__init__()
-        # Hyper-parameter
-        self.L, self.N = L, N
-        # Components
-        # 50% overlap
-        self.conv1d_U = nn.Conv1d(1, N, kernel_size=L, stride=L // 2, bias=False)
-
-    def forward(self, mixture):
-        """
-        Args:
-            mixture: [M, T], M is batch size, T is #samples
-        Returns:
-            mixture_w: [M, N, K], where K = (T-L)/(L/2)+1 = 2T/L-1
-        """
-        mixture = torch.unsqueeze(mixture, 1)  # [M, 1, T]
-        mixture_w = F.relu(self.conv1d_U(mixture))  # [M, N, K]
-        return mixture_w
-
-
-class Decoder(nn.Module):
-    def __init__(self, N, L):
-        super(Decoder, self).__init__()
-        # Hyper-parameter
-        self.N, self.L = N, L
-        # Components
-        self.basis_signals = nn.Linear(N, L, bias=False)
-
-    def forward(self, mixture_w, est_mask):
-        """
-        Args:
-            mixture_w: [M, N, K]
-            est_mask: [M, C, N, K]
-        Returns:
-            est_source: [M, C, T]
-        """
-        # D = W * M
-        source_w = torch.unsqueeze(mixture_w, 1) * est_mask  # [M, C, N, K]
-        source_w = torch.transpose(source_w, 2, 3) # [M, C, K, N]
-        # S = DV
-        est_source = self.basis_signals(source_w)  # [M, C, K, L]
-        est_source = overlap_and_add(est_source, self.L//2) # M x C x T
-        return est_source
-
 
 class AttractorConvNet(nn.Module):
     def __init__(self, N, B, H, P, X, R, C, K, emb_size, norm_type="gLN", causal=False,
@@ -229,7 +191,7 @@ class AttractorConvNet(nn.Module):
         M, N, K = mixture_w.size()
         feat = self.network(mixture_w)  # [M, N, K] -> [M, N*emb_size, K]
         embedding = feat.view(M, self.emb_size, N, K) # [M, N*emb_size, K] -> [M, emb_size, N, K]
-        score = self.cluster(embedding)
+        score = self.cluster(embedding) # [M, emb_size, N, K ] -> [M, C, N, K ]
         if self.mask_nonlinear == 'softmax':
             est_mask = F.softmax(score, dim=1)
         elif self.mask_nonlinear == 'relu':
@@ -239,182 +201,4 @@ class AttractorConvNet(nn.Module):
         else:
             raise ValueError("Unsupported mask non-linear function")
         return est_mask
-
-
-class TemporalBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, norm_type="gLN", causal=False, dropout=0.0):
-        super(TemporalBlock, self).__init__()
-        # [M, B, K] -> [M, H, K]
-        conv1x1 = nn.Conv1d(in_channels, out_channels, 1, bias=False)
-        prelu = nn.PReLU()
-        norm = chose_norm(norm_type, out_channels)
-        # [M, H, K] -> [M, B, K]
-        dsconv = DepthwiseSeparableConv(out_channels, in_channels, kernel_size,
-                                        stride, padding, dilation, norm_type,
-                                        causal)
-        d = nn.Dropout(dropout)
-        # Put together
-        self.net = nn.Sequential(conv1x1, prelu, norm, d, dsconv)
-
-    def forward(self, x):
-        """
-        Args:
-            x: [M, B, K]
-        Returns:
-            [M, B, K]
-        """
-        residual = x
-        out = self.net(x)
-        # TODO: when P = 3 here works fine, but when P = 2 maybe need to pad?
-        return out + residual  # look like w/o F.relu is better than w/ F.relu
-
-class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, norm_type="gLN", causal=False):
-        super(DepthwiseSeparableConv, self).__init__()
-        # Use `groups` option to implement depthwise convolution
-        # [M, H, K] -> [M, H, K]
-        depthwise_conv = nn.Conv1d(in_channels, in_channels, kernel_size,
-                                   stride=stride, padding=padding,
-                                   dilation=dilation, groups=in_channels,
-                                   bias=False)
-        if causal:
-            chomp = Chomp1d(padding)
-        prelu = nn.PReLU()
-        norm = chose_norm(norm_type, in_channels)
-        # [M, H, K] -> [M, B, K]
-        pointwise_conv = nn.Conv1d(in_channels, out_channels, 1, bias=False)
-        # Put together
-        if causal:
-            self.net = nn.Sequential(depthwise_conv, chomp, prelu, norm,
-                                     pointwise_conv)
-        else:
-            self.net = nn.Sequential(depthwise_conv, prelu, norm,
-                                     pointwise_conv)
-
-    def forward(self, x):
-        """
-        Args:
-            x: [M, H, K]
-        Returns:
-            result: [M, B, K]
-        """
-        return self.net(x)
-
-
-class Chomp1d(nn.Module):
-    """To ensure the output length is the same as the input.
-    """
-    def __init__(self, chomp_size):
-        super(Chomp1d, self).__init__()
-        self.chomp_size = chomp_size
-
-    def forward(self, x):
-        """
-        Args:
-            x: [M, H, Kpad]
-        Returns:
-            [M, H, K]
-        """
-        return x[:, :, :-self.chomp_size].contiguous()
-
-
-def chose_norm(norm_type, channel_size):
-    """The input of normlization will be (M, C, K), where M is batch size,
-       C is channel size and K is sequence length.
-    """
-    if norm_type == "gLN":
-        return GlobalLayerNorm(channel_size)
-    elif norm_type == "cLN":
-        return ChannelwiseLayerNorm(channel_size)
-    else: # norm_type == "BN":
-        # Given input (M, C, K), nn.BatchNorm1d(C) will accumulate statics
-        # along M and K, so this BN usage is right.
-        return nn.BatchNorm1d(channel_size)
-
-
-# TODO: Use nn.LayerNorm to impl cLN to speed up
-class ChannelwiseLayerNorm(nn.Module):
-    """Channel-wise Layer Normalization (cLN)"""
-    def __init__(self, channel_size):
-        super(ChannelwiseLayerNorm, self).__init__()
-        self.gamma = nn.Parameter(torch.Tensor(1, channel_size, 1))  # [1, N, 1]
-        self.beta = nn.Parameter(torch.Tensor(1, channel_size,1 ))  # [1, N, 1]
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        self.gamma.data.fill_(1)
-        self.beta.data.zero_()
-
-    def forward(self, y):
-        """
-        Args:
-            y: [M, N, K], M is batch size, N is channel size, K is length
-        Returns:
-            cLN_y: [M, N, K]
-        """
-        mean = torch.mean(y, dim=1, keepdim=True)  # [M, 1, K]
-        var = torch.var(y, dim=1, keepdim=True, unbiased=False)  # [M, 1, K]
-        cLN_y = self.gamma * (y - mean) / torch.pow(var + EPS, 0.5) + self.beta
-        return cLN_y
-
-
-class GlobalLayerNorm(nn.Module):
-    """Global Layer Normalization (gLN)"""
-    def __init__(self, channel_size):
-        super(GlobalLayerNorm, self).__init__()
-        self.gamma = nn.Parameter(torch.Tensor(1, channel_size, 1))  # [1, N, 1]
-        self.beta = nn.Parameter(torch.Tensor(1, channel_size,1 ))  # [1, N, 1]
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        self.gamma.data.fill_(1)
-        self.beta.data.zero_()
-
-    def forward(self, y):
-        """
-        Args:
-            y: [M, N, K], M is batch size, N is channel size, K is length
-        Returns:
-            gLN_y: [M, N, K]
-        """
-        # TODO: in torch 1.0, torch.mean() support dim list
-        mean = y.mean(dim=1, keepdim=True).mean(dim=2, keepdim=True) #[M, 1, 1]
-        var = (torch.pow(y-mean, 2)).mean(dim=1, keepdim=True).mean(dim=2, keepdim=True)
-        gLN_y = self.gamma * (y - mean) / torch.pow(var + EPS, 0.5) + self.beta
-        return gLN_y
-
-
-if __name__ == "__main__":
-    torch.manual_seed(123)
-    M, N, L, T = 2, 3, 4, 12
-    K = 2*T//L-1
-    B, H, P, X, R, C, norm_type, causal = 2, 3, 3, 3, 2, 2, "gLN", False
-    mixture = torch.randint(3, (M, T))
-    # test Encoder
-    encoder = Encoder(L, N)
-    encoder.conv1d_U.weight.data = torch.randint(2, encoder.conv1d_U.weight.size())
-    mixture_w = encoder(mixture)
-    print('mixture', mixture)
-    print('U', encoder.conv1d_U.weight)
-    print('mixture_w', mixture_w)
-    print('mixture_w size', mixture_w.size())
-
-    # test TemporalConvNet
-    separator = TemporalConvNet(N, B, H, P, X, R, C, norm_type=norm_type, causal=causal)
-    est_mask = separator(mixture_w)
-    print('est_mask', est_mask)
-
-    # test Decoder
-    decoder = Decoder(N, L)
-    est_mask = torch.randint(2, (B, K, C, N))
-    est_source = decoder(mixture_w, est_mask)
-    print('est_source', est_source)
-
-    # test Conv-TasNet
-    conv_tasnet = ConvTasNet(N, L, B, H, P, X, R, C, norm_type=norm_type)
-    est_source = conv_tasnet(mixture)
-    print('est_source', est_source)
-    print('est_source size', est_source.size())
 
