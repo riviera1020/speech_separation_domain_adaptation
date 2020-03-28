ecd4f75da6446b5153f2b839e3faf8a33b84b832
riviera1020 <riviera1020@gmail.com>
2020-03-28 22:01:39 +0800

    wham preprocess & dataset done; accept dynamic scaling but not yet check sdr

--- data/wham/run.sh
+++ data/wham/run.sh
@@ -1,7 +1,12 @@
 
-mkdir -p ./data/wham/id_list
+#mkdir -p ./data/wham/id_list
 
-python ./data/wham/preprocess.py \
-    --in_dir /home/riviera1020/Big/Corpus/wham-mix/wav8k/min/ \
-    --out_dir ./data/wham/id_list
+#python ./data/wham/preprocess.py \
+#    --in_dir /home/riviera1020/Big/Corpus/wham-mix/wav8k/min/ \
+#    --out_dir ./data/wham/id_list
 
+mkdir -p ./data/wham/noise_id_list
+python ./data/wham/fetch_scale.py \
+    --noise_dir /home/riviera1020/Big/Corpus/wham_noise/ \
+    --wsj0_dir /home/riviera1020/Big/Corpus/wsj0-mix/ \
+    --out_dir ./data/wham/noise_id_list
