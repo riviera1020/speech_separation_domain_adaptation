3d4330be4c500c1271887e430596e3e2a09955ae
riviera1020 <riviera1020@gmail.com>
2020-04-08 17:46:12 +0800

    mcd bug fix

--- src/train_mcd.py
+++ src/train_mcd.py
@@ -195,7 +195,7 @@ class Trainer(Solver):
                     weight_decay = weight_decay)
         elif optim_type == 'ranger':
             opt = Ranger(
-                    parameters(),
+                    parameters,
                     lr = lr,
                     weight_decay = weight_decay)
         else:
